32230a4b4b1055a165e615343b9ab199de1fa0e1
git <wkaifeng2007@163.com>
2021-11-19 16:19:52 +0800

    midified service port

--- RunIt/app/api/is_alive.py
+++ RunIt/app/api/is_alive.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from loguru import logger
 from utils.services.base.api_base import msg
-from handler.create_handler import *
 from utils.xauth.antx_auth import verification
+from handler.alive_handler import *
 
 router = APIRouter(dependencies=[Depends(verification)])
 # router = APIRouter()
--- RunIt/app/models/task_models.py
+++ RunIt/app/models/task_models.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 from typing import Optional
+import time
 
 class TaskCreate(BaseModel):
-    pass
+    room: str
+    task_name: Optional[str] = f'RUNIT-{int(time.time())}'
+    mode: str = 'record'
+
+class TaskStop(BaseModel):
+    task_id: str
