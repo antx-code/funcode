e966d1ffb2540a53604ce10db49514ea365a1bfa
git <wkaifeng2007@163.com>
2021-11-19 11:35:19 +0800

    add init for runit utils

--- RunIt/app/api/is_alive.py
+++ RunIt/app/api/is_alive.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
+from loguru import logger
 from utils.services.base.api_base import msg
 from handler.create_handler import *
-from utils.auth.xauth import verification
+from utils.xauth.antx_auth import verification
 
 router = APIRouter(dependencies=[Depends(verification)])
 # router = APIRouter()
